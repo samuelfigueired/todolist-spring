b50f7bbe68d9984c87e564164a25c5f4982bcf42
Samuel Maciel S. Figueiredo <samuelfigsre@gmail.com>
2025-02-21 21:41:17 -0300

    Inserção de errors

---
+++ src/main/java/br/com/battcompany/todolist/errors/ExceptionHandlerController.java
@@ -0,0 +1,16 @@
+package br.com.battcompany.todolist.errors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+
+@ControllerAdvice
+public class ExceptionHandlerController {
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMostSpecificCause().getMessage());
+    }
+}
--- src/main/java/br/com/battcompany/todolist/task/TaskController.java
+++ src/main/java/br/com/battcompany/todolist/task/TaskController.java
@@ -1,6 +1,7 @@
 package br.com.battcompany.todolist.task;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import br.com.battcompany.todolist.utils.Utils;
+
 
 @RestController
 @RequestMapping("/tasks")
@@ -37,6 +44,32 @@ public class TaskController {
         }
         var task = this.taskRepository.save(taskModel);
         return ResponseEntity.status(HttpStatus.OK).body(task);
+    }
+
+    @GetMapping("/") //Listar todas as tarefas
+    public List<TaskModel> list(HttpServletRequest request){
+        var idUser = request.getAttribute("idUser");
+        var tasks = this.taskRepository.findByIdUser((UUID)idUser);
+        return tasks;
+    }
+    //http//localhost:8080/tasks//5628-452csdvds
+    @PutMapping("/{id}") //Podendo atualizar uma tarefa
+    public ResponseEntity update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id){ //Http servelet pois precisamos que o usuario esteja autenticadp
+        //update
+        var task = this.taskRepository.findById(id).orElse(null); //puxar nno banco de dados
+        
+        if (task == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Tarefa não encontrada!");
+        }   
+
+        var idUser = request.getAttribute("idUser");
+
+        if(!task.getIdUser().equals(idUser)){ //Verifica se o usuario é o mesmo que criou a tarefa
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário não autorizado a alterar a tarefa!");
+        }
+        Utils.copyNonNullProperties(taskModel, task);
+        var taskUpdated = this.taskRepository.save(task);
+        return ResponseEntity.status(HttpStatus.OK).body(this.taskRepository.save(taskUpdated));
     }
 }
  
